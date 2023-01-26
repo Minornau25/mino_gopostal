f735cdadfeb3b45b88124caeec3960d1535a555f
Minornau25 <77509459+Minornau25@users.noreply.github.com>
2023-01-27 00:46:35 +0100

    Delete minogopostal.sql

--- mino_gopostal/minogopostal.sql
+++
@@ -1,9 +0,0 @@
-USE `es_extended`;
-
-INSERT INTO `jobs` (name, label) VALUES
-	('gopostal','Dealer')
-;
-
-INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('gopostal',0,'worker','Dealer',500,'{}','{}')
-;
